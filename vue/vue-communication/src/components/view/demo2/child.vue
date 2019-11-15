f82180a173a40f4a0156127aadfcf445dcfc46e3
LADY6 <1790212172@qq.com>
2019-11-15 21:23:03 +0800

    vue

---
+++ vue/vue-communication/src/components/view/demo2/child.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class='child'>
+    <h2>子组件</h2>
+    <p>从父组件接收到的数据是:</p>
+    <div>
+      String: <span class='red'>{{sendMsg}}</span>
+    </div>
+    <div>
+      Number: <span class='red'>{{sendNum}}</span>
+    </div>
+    <ul class='red'>
+        <li v-for='item in sendObj' :key='item.id'>{{item.name}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sendMsg: {
+      type: String,
+      require: true
+    },
+    sendNum: {
+      type: Number,
+      require: false
+    },
+    sendObj: {
+      // 声明验证类型
+      validator (val) {
+        if (
+          Object.prototype.toString.call(val) === '[object Array]' ||
+          Object.prototype.toString.call(val) === '[object Object]'
+        ) {
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
---
+++ vue/vue-communication/src/components/view/demo3/father.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h2>父组件</h2>
+    <p>
+      从子组件获取的消息为:
+      <span class="red">{{msg}}</span>
+    </p>
+    <child v-on:messgae="getMsg"></child>
+  </div>
+</template>
+
+<script>
+import child from './child'
+export default {
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  components: {
+    child
+  },
+  methods: {
+    getMsg (e) {
+      this.msg = e
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
